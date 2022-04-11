c2d1231c76d626e2b8cf865609d59bab8df02a5e
Rohan Sikder <Rohansikder4@gmail.com>
2022-04-11 22:07:47 +0100

    Added delete client,display client and print all client details to report file

--- main.c
+++ main.c
@@ -26,6 +26,13 @@ typedef struct node
 //Define Functions
 void addAtTheStartList(struct node** top);
 void addAtTheEndList(struct node* top);
+void displayList(nodeT* top);
+int search(nodeT* top, int searchCRN);
+void deleteAtEnd(struct node* top);
+void deleteAtStart(struct node** top);
+void deleteAtLocation(struct node* top, int location);
+int listLength(nodeT* top);
+void outputToFile(struct node* top);
 
 
 void main()
@@ -34,26 +41,30 @@ void main()
 	nodeT* newNode;
 	nodeT* temp;
 	int choice;
+	char searchCo[30];
+	int resultSearch;
+	int resultDelete;
+	int searchID;
+	int deleteId;
 
-	printf("Please enter 1 to Add client (Note: Company Registeration Number must be unique)\n");
-	printf("Please enter 2 to Display all client details\n");
+
+	printf("Please enter 1 to Add client (Note: Company Registeration Number must be unique)\n");//Done But not unique
+	printf("Please enter 2 to Display all client details\n");//Done
 	printf("Please enter 3 to Display client details\n");
 	printf("Please enter 4 to Update a client details\n");
-	printf("Please enter 5 to Delete client\n");
-	printf("Please enter 6 to Generate statistics (a to c) based on the user selecting one of the criteria listed in I - II\n---A. Less than €1 Million\n---B. Less than €10 Million\n---C. Over €10 Million\n---I. Area of Company Sales\n---II. Number of Employees\n");
-	printf("\nPlease enter 7 to Print all client details into a report file.\n");
+	printf("Please enter 5 to Delete client\n");//Done
+	printf("Please enter 6 to Generate statistics (a to c) based on the user selecting one of the criteria listed in I - II\n\tA. Less than €1 Million\n\tB. Less than €10 Million\n\tC. Over €10 Million\n\tI. Area of Company Sales\n\tII. Number of Employees\n");
+	printf("Please enter 7 to Print all client details into a report file.\n");//Done
 	printf("Please enter 8 to List all the clients in order of their last average turnover\n");
 	printf("Please enter -1 to exit application\n");
+	printf("\n");
 	scanf("%d", &choice);
+	printf("\n");
+
 
 	while (choice != -1)
 	{
 		if (choice == 1)
-		{
-			addAtTheStartList(&headPtr);
-		}
-
-		else if (choice == 2)
 		{
 			if (headPtr == NULL)
 			{
@@ -66,84 +77,106 @@ void main()
 			}
 		}
 
+		else if (choice == 2)
+		{
+			displayList(headPtr);
+		}
+
 		else if (choice == 3)
 		{
-			
+			printf("Please enter the Company Resgistration number you wish to search for\n");
+			scanf("%d", &searchID);
+
+			resultSearch = search(headPtr, searchID);
+
+			if (resultSearch == -1) {
+				printf("The Company %d does not exist in the list\n", searchID);
+			}
+			else {
+				printf("The Company %d is at location %d\n", searchID, resultSearch);
+			}
 		}
 
 		else if (choice == 4)
 		{
-			
+
 		}
 
 		else if (choice == 5)
 		{
+			printf("Please enter the Company Registration Number To delete all client information from database:\n");
+			scanf("%d", &deleteId);
 
-			
+			resultDelete = search(headPtr, deleteId);
+
+			if (headPtr == NULL)
+			{
+				printf("The database is empty\n");
+			}
+			else if (resultDelete<0 || resultDelete>(listLength(headPtr) - 1))
+			{
+				printf("The CRN is not valid\n");
+			}
+
+			else if (resultDelete == 0)
+			{
+				deleteAtStart(&headPtr);
+			}
+
+			else if (resultDelete == (listLength(headPtr) - 1))
+			{
+				deleteAtEnd(headPtr);
+			}
+
+			else
+			{
+				deleteAtLocation(headPtr, resultDelete);
+			}
+
 		}
 
 		else if (choice == 6)
 		{
-			
+
 		}
 
 		else if (choice == 7)
 		{
-			
+			outputToFile(headPtr);
 		}
 
 		else if (choice == 8)
 		{
-			
+
 		}
 
 
-		printf("Please enter 1 to Add client (Note: Company Registeration Number must be unique)\n");
+		printf("\nPlease enter 1 to Add client (Note: Company Registeration Number must be unique)\n");
 		printf("Please enter 2 to Display all client details\n");
 		printf("Please enter 3 to Display client details\n");
 		printf("Please enter 4 to Update a client details\n");
 		printf("Please enter 5 to Delete client\n");
-		printf("Please enter 6 to Generate statistics (a – c) based on the user selecting one of the criteria listed in I - II\n---A. Less than €1 Million\n---B. Less than €10 Million\n---C. Over €10 Million\n---I. Area of Company Sales\n---II. Number of Employees\n");
-		printf("\nPlease enter 7 to Print all client details into a report file.\n");
+		printf("Please enter 6 to Generate statistics (a – c) based on the user selecting one of the criteria listed in I - II\n\tA. Less than €1 Million\n\tB. Less than €10 Million\n\tC. Over €10 Million\n\tI. Area of Company Sales\n\tII. Number of Employees\n");
+		printf("Please enter 7 to Print all client details into a report file.\n");
 		printf("Please enter 8 to List all the clients in order of their last average turnover\n");
 		printf("Please enter -1 to exit application\n");
 		scanf("%d", &choice);
-	}
+		printf("\n");
+	}//End OF while
 
 }//End Of Main
 
 void addAtTheEndList(struct node* top)
 {
+
 	struct node* temp = top;
 	struct node* newNode;
 
 	newNode = (struct node*)malloc(sizeof(struct node));
 	printf("Please enter Company Registration Number(CRN):\n");
-	scanf("%d",&newNode->CRN);
-	printf("\nPlease enter Company Name:\n");
+	scanf("%d", &newNode->CRN);
+	printf("Please enter Company Name:\n");
 	scanf("%s", newNode->CoName);
-	printf("\nPlease enter Company Country:\n");
-	scanf("%s", newNode->CoCountry);
-	printf("\nPlease enter Year Company was Founded:\n");
-	scanf("%d", &newNode->yearFounded);
-	printf("\nPlease enter Company Email Addres(Must be valid email!):\n");
-	scanf("%s", newNode->email);
-	printf("Please enter Company Contact Name:\n");
-	scanf("%s", newNode->ContactName);
-	printf("\nPlease enter Last Order:\n");
-	scanf("%d", &newNode->LastOrder);
-	printf("\nPlease enter Number Of Employees:\n");
-	scanf("%d", &newNode->NumOfEmployees);
-	printf("\nPlease enter Average Annual Order:\n");
-	scanf("%f", &newNode->AverageAnnualOrder);
-	printf("\nIs the Company Vat Registered?(Please Enter 1 for Yes or 2 for No.):\n");
-	scanf("%d", &newNode->VatReg);
-	printf("\nWhat is the Company's Average Turnover?:\n-Please enter 1 For Less than €1 Million\n-Please enter 2 for Less than €10 Million\n-Please enter 3 for Over €10 Million\n");
-	scanf("%d", &newNode->AvgTurnover);
-	printf("\nHow many staff are employed in the Company?:\n-Please enter 1 For Less than 10\n-Please enter 2 for Less than 100\n-Please enter 3 for Over 100\n");
-	scanf("%d", &newNode->StaffNum);
-	printf("\nWhich area of sales is the company?:\n-Please enter 1 For ICT\n-Please enter 2 for Medical Devices\n-Please enter 3 for Other area\n");
-	scanf("%d", &newNode->AreaOfSales);
 
 	while (temp->NEXT != NULL)
 	{
@@ -152,41 +185,159 @@ void addAtTheEndList(struct node* top)
 
 	newNode->NEXT = NULL;
 	temp->NEXT = newNode;
-}
+
+
+}//End of addAtEndList
 
 void addAtTheStartList(struct node** top)
 {
+
 	struct node* newNode;
 
 	newNode = (struct node*)malloc(sizeof(struct node));
+
 	printf("Please enter Company Registration Number(CRN):\n");
 	scanf("%d", &newNode->CRN);
-	printf("\nPlease enter Company Name:\n");
+	printf("Please enter Company Name:\n");
 	scanf("%s", newNode->CoName);
-	printf("\nPlease enter Company Country:\n");
-	scanf("%s", newNode->CoCountry);
-	printf("\nPlease enter Year Company was Founded:\n");
-	scanf("%d", &newNode->yearFounded);
-	printf("\nPlease enter Company Email Addres(Must be valid email!):\n");
-	scanf("%s", newNode->email);
-	printf("Please enter Company Contact Name:\n");
-	scanf("%s", newNode->ContactName);
-	printf("\nPlease enter Last Order:\n");
-	scanf("%d", &newNode->LastOrder);
-	printf("\nPlease enter Number Of Employees:\n");
-	scanf("%d", &newNode->NumOfEmployees);
-	printf("\nPlease enter Average Annual Order:\n");
-	scanf("%f", &newNode->AverageAnnualOrder);
-	printf("\nIs the Company Vat Registered?(Please Enter 1 for Yes or 2 for No.):\n");
-	scanf("%d", &newNode->VatReg);
-	printf("\nWhat is the Company's Average Turnover?:\n-Please enter 1 For Less than €1 Million\n-Please enter 2 for Less than €10 Million\n-Please enter 3 for Over €10 Million\n");
-	scanf("%d", &newNode->AvgTurnover);
-	printf("\nHow many staff are employed in the Company?:\n-Please enter 1 For Less than 10\n-Please enter 2 for Less than 100\n-Please enter 3 for Over 100\n");
-	scanf("%d", &newNode->StaffNum);
-	printf("\nWhich area of sales is the company?:\n-Please enter 1 For ICT\n-Please enter 2 for Medical Devices\n-Please enter 3 for Other area\n");
-	scanf("%d", &newNode->AreaOfSales);
+
 
 	newNode->NEXT = *top;
 	*top = newNode;
 
+}//End of addAtStartList
+
+void displayList(nodeT* top)
+{
+	struct node* temp;
+
+	temp = top;
+
+	while (temp != NULL)
+	{
+		printf("Company Registration Number(CRN) is: %d\n", temp->CRN);
+		printf("Company Name is %s.\n", temp->CoName);
+		printf("Company's is located in: %s.\n", temp->CoCountry);
+		printf("Year Company was founded in %d.\n", temp->yearFounded);
+		printf("Company Email Address is %s.\n", temp->email);
+		printf("Company Contact Name is %s.\n", temp->ContactName);
+		printf("Company's Last order was %d.\n", temp->LastOrder);
+		printf("Company has %d employees.\n", temp->NumOfEmployees);
+		printf("Company's average annual order is %f\n", temp->AverageAnnualOrder);
+		printf("Is Company Vat Registered (1 == Yes and 2 == No) : %d\n", temp->VatReg);
+		printf("Companys Average Turnover (1 == Less than 1 Million, 2 == Less than 10 Million, 3 == Over 10 Million): %d\n", temp->AvgTurnover);
+		printf("Number of Staff employeed at company is (1 == Less than 10, 2 == Less than 100, 3 == Over 100): %d\n", temp->StaffNum);
+		printf("Area of sales of company is (1 == ICT, 2 == Medical Devices, 3 == Over Area): %d\n", temp->AreaOfSales);
+
+		temp = temp->NEXT;
+	}
+}//End of displayLiST
+
+int search(nodeT* top, int searchCRN)
+{
+	struct node* temp = top;
+	int found = -1;
+	int count = 0;
+
+	while (temp != NULL)
+	{
+		if (temp->CRN == searchCRN)
+		{
+			found = count;
+		}
+
+		temp = temp->NEXT;
+		count++;
+	}
+
+	return found;
+}//End of search
+
+void outputToFile(struct node* top)
+{
+	struct node* temp;
+	FILE* fp;
+	temp = top;
+
+	fp = fopen("clientDataBase.txt", "w");
+
+	while (temp != NULL)
+	{
+		if (fp != NULL)
+		{
+			fprintf(fp, "Company Registration Number(CRN) is: %d\n", temp->CRN);
+			fprintf(fp, "Company Name is %s.\n", temp->CoName);
+			fprintf(fp, "Company's is located in: %s.\n", temp->CoCountry);
+			fprintf(fp, "Year Company was founded in %d.\n", temp->yearFounded);
+			fprintf(fp, "Company Email Address is %s.\n", temp->email);
+			fprintf(fp, "Company Contact Name is %s.\n", temp->ContactName);
+			fprintf(fp, "Company's Last order was %d.\n", temp->LastOrder);
+			fprintf(fp, "Company has %d employees.\n", temp->NumOfEmployees);
+			fprintf(fp, "Company's average annual order is %f\n", temp->AverageAnnualOrder);
+			fprintf(fp, "Is Company Vat Registered (1 == Yes and 2 == No) : %d\n", temp->VatReg);
+			fprintf(fp, "Companys Average Turnover (1 == Less than 1 Million, 2 == Less than 10 Million, 3 == Over 10 Million): %d\n", temp->AvgTurnover);
+			fprintf(fp, "Number of Staff employeed at company is (1 == Less than 10, 2 == Less than 100, 3 == Over 100): %d\n", temp->StaffNum);
+			fprintf(fp, "Area of sales of company is (1 == ICT, 2 == Medical Devices, 3 == Over Area): %d\n", temp->AreaOfSales);
+		}
+		temp = temp->NEXT;
+	}
+
+	if (fp != NULL)
+	{
+		fclose(fp);
+	}
+}
+
+int listLength(nodeT* top)
+{
+	struct node* temp = top;
+	int count = 0;
+
+	while (temp != NULL)
+	{
+		count = count + 1;
+		temp = temp->NEXT;
+	}
+
+	return count;
+}
+
+void deleteAtEnd(struct node* top)
+{
+	struct node* temp = top;
+	struct node* prev = NULL;
+
+	while (temp->NEXT != NULL)
+	{
+		prev = temp;
+		temp = temp->NEXT;
+	}
+
+	prev->NEXT = NULL;
+	free(temp);
+}
+
+void deleteAtStart(struct node** top)
+{
+	struct node* temp = *top;
+
+	*top = (*top)->NEXT;
+
+	free(temp);
+}
+
+void deleteAtLocation(struct node* top, int location)
+{
+	struct node* temp = top;
+	struct node* prev;
+	int i;
+
+	for (i = 0; i < location; i++)
+	{
+		prev = temp;
+		temp = temp->NEXT;
+	}
+
+	prev->NEXT = temp->NEXT;
+	free(temp);
 }
